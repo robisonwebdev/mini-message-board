13973b6c87a6ed937a7936f40ce2a1a502d13977
robisonwebdev <david@robisonweb.dev>
2021-09-06 15:58:21 -0600

    Added messages array that contain objects.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date()
+  },
+  {
+    text: 'Good morning!',
+    user: 'Patrick',
+    added: new Date()
+  },
+  {
+    text: 'Good night!',
+    user: 'Kristen',
+    added: new Date()
+  }
+]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
